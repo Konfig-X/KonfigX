281aeeed98d1d29fa23710a631fe46af090c62b0
Dani Arifin <danithesillyfem@gmail.com>
2026-06-07 09:43:52 +0700

    Update mssi.sh

--- konfigx/configs/mssi.sh
+++ konfigx/configs/mssi.sh
@@ -4,7 +4,7 @@
 # UN1CA configuration file for MediaTek devices (mssi)
 
 # Inherit source firmware configuration from essi
-source "$SRC_DIR/unica/configs/essi.sh" || return 1
+source "$SRC_DIR/konfigx/configs/essi.sh" || return 1
 
 # Galaxy A34 5G (One UI 8.0)
 SOURCE_EXTRA_FIRMWARES=("SM-A346B/EUX/351648441234565")
