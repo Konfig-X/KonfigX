741dbd69bf5b5ad31dc6738d5abd03309906ba79
DaniTheSillyFem <danithesillyfem@gmail.com>
2026-06-11 18:01:43 +0700

    fix signature patching

--- konfigx/patches/signature/customize.sh
+++ konfigx/patches/signature/customize.sh
@@ -9,8 +9,8 @@ APPLY_PATCH "system" "system/framework/services.jar" "$MODPATH/services.jar/0001
 
 CERT_SIGNATURE="$(sed "/CERTIFICATE/d" "$SRC_DIR/security/${CERT_PREFIX}_platform.x509.pem" | tr -d "\n" | base64 -d | xxd -p -c 0)"
 SMALI_PATCH "system" "system/framework/services.jar" \
-    "smali_classes2/com/android/server/pm/InstallPackageHelper.smali" "replace" \
-    '<init>(Lcom/android/server/pm/PackageManagerService;Lcom/android/server/pm/AppDataHelper;Lcom/android/server/pm/RemovePackageHelper;Lcom/android/server/pm/DeletePackageHelper;Lcom/android/server/pm/BroadcastHelper;)V' \
+    "smali_classes2/com/android/server/pm/ScanPackageUtils.smali" "replace" \
+    'signedWithCustomSignature([Landroid/content/pm/Signature;)Z' \
     'CONFIG_CUSTOM_PLATFORM_SIGNATURE' \
     "$CERT_SIGNATURE" \
     > /dev/null
