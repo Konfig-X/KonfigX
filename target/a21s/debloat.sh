0275c77d2b84d40bd121379929ca3929aa1d826f
Daniella :3 <danithesillyfem@gmail.com>
2026-06-18 12:46:08 +0700

    platform: exynos850: add debloat.sh

---
+++ target/a21s/debloat.sh
@@ -0,0 +1,44 @@
+# Copyright (C) 2025 BasGame1
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# Debloat list for Samsung Exynos 850 devices (exynos850)
+# - Add entries inside the specific partition containing that file (<PARTITION>_DEBLOAT+="")
+# - DO NOT add the partition name at the start of any entry (eg. "/system/dpolicy_system")
+# - DO NOT add a slash at the start of any entry (eg. "/dpolicy_system")
+
+# Camera SDK
+SYSTEM_DEBLOAT+="
+system/etc/default-permissions/default-permissions-com.samsung.android.globalpostprocmgr.xml
+system/etc/default-permissions/default-permissions-com.samsung.petservice.xml
+system/etc/default-permissions/default-permissions-com.samsung.videoscan.xml
+system/etc/default-permissions/default-permissions-com.sec.factory.cameralyzer.xml
+system/etc/permissions/cameraservice.xml
+system/etc/permissions/privapp-permissions-com.samsung.android.globalpostprocmgr.xml
+system/etc/permissions/privapp-permissions-com.samsung.petservice.xml
+system/etc/permissions/privapp-permissions-com.samsung.videoscan.xml
+system/etc/permissions/sec_camerax_impl.xml
+system/etc/permissions/sec_camerax_service.xml
+system/framework/sec_camerax_impl.jar
+system/framework/scamera_sep.jar
+system/priv-app/GlobalPostProcMgr
+system/priv-app/PetService
+system/priv-app/SCameraSDKService
+system/priv-app/sec_camerax_service
+system/priv-app/VideoScan
+"
+
+# Overlays
+PRODUCT_DEBLOAT+="
+overlay/SoftapOverlayDualAp
+overlay/SoftapOverlayOWE
+"
+
+# StorageShare (kSMBd)
+SYSTEM_DEBLOAT+="
+system/etc/default-permissions/default-permissions-com.samsung.android.hwresourceshare.storage.xml
+system/etc/init/ksmbd.rc
+system/etc/permissions/privapp-permissions-com.samsung.android.hwresourceshare.storage.xml
+system/etc/sysconfig/preinstalled-packages-com.samsung.android.hwresourceshare.storage.xml
+system/etc/ksmbd.conf
+system/priv-app/StorageShare
+"
